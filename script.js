5214289aab056d40d745e76555417fd7bb728043
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-06 12:05:47 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 //your JS code here. If required.
-let div=document.createElement("div");
-div.id='browser-info';
-document.body.append(div);
+let div=document.getElementById("browser-info");
+ 
 div.innerHTML="You are using "+navigator.appName+" version "+navigator.appVersion;
